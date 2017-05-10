bd5b7767c4b00a0ea44abb12728646df41d024f7
Emir Tola <emir.tola94@gmail.com>
2017-05-10 12:49:39 +0200

    Direction to walk will be find at start of solving.

--- src/ss17/algodat/maze/tremaux/Tremaux.cpp
+++ src/ss17/algodat/maze/tremaux/Tremaux.cpp
@@ -10,13 +10,28 @@ int Tremaux::solve() {
     // Find starting point and direction
     int currX = 0;
     int currY = 0;
+
+    int directionX = 0;
+    int directionY = 0;
+
     for (int i = 0; i < ROWS; i++) {
-        std::cout << "row: " << i << "\n";
+        std::cout << "row: " << i << std::endl;
         for (int j = 0; j < COLUMNS; j++) {
-            if (maze[i*j + i] == 'S') {
+            if (maze[i*COLUMNS + j] == 'S') {
                 currX = i;
                 currY = j;
 
+                if (i != ROWS-1 && maze[(i+1)*COLUMNS + j] == '.') {
+                    directionY = 1;
+                } else if (i != 0 && maze[(i-1)*COLUMNS + j] == '.') {
+                    directionY = -1;
+                } else if (j != COLUMNS-1 && maze[i*COLUMNS + j+1] == '.') {
+                    directionX = 1;
+                } else if (j != 0 && maze[i*COLUMNS + j-1] == '.') {
+                    directionX = -1;
+                }
+                
+                
                 // Completely break out of nested loop.
                 i = ROWS;
                 j = COLUMNS;
@@ -24,10 +39,18 @@ int Tremaux::solve() {
             }
         }
     }
+    
+    std::cout << "dirX: " << directionX << ", dirY: " << directionY << std::endl;
 
 
+    // Solve the maze
 
-    // Solve the maze.
+    bool notSolved = true;
+
+//    while (notSolved) {
+//
+//    }
+
     for (int i = 0; i < ROWS; i++) {
         for (int j = 0; j < COLUMNS; j++) {
 
