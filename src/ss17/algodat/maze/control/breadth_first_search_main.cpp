3286caa05c975a2c37720af8c5cca4dea4f3c4da
Loderer, Rudi <loderer@hm.edu>
2017-06-05 15:29:48 +0200

    fix lost dependencies

--- src/ss17/algodat/maze/control/breadth_first_search_main.cpp
+++ src/ss17/algodat/maze/control/breadth_first_search_main.cpp
@@ -4,9 +4,55 @@
 
 #include <iostream>
 #include "shortest_path/breadth_first_search/BreathFirstSearch.h"
+#include "shortest_path/breadth_first_search/BreathFirstSearchLib.h"
+
+void printString(const int ROWS, const int COLUMNS, const string &solution);
 
 int main(){
-    BreathFirstSearch *bfs = new BreathFirstSearch();
-    std::cout<<bfs->getHello()<<std::endl;
+    const int ROWS = 15;
+    const int COLUMNS = 15;
+
+    char maze[] = {
+            '#','S','#','#','#','#','#','#','#','#','#','#','#','#','#',
+            '#','.','.','.','.','.','.','.','#','.','.','.','.','.','#',
+            '#','.','#','#','#','#','#','.','#','#','#','#','#','.','#',
+            '#','.','#','#','#','#','#','.','#','#','#','#','#','.','#',
+            '#','.','.','.','.','.','.','.','.','.','.','.','.','.','#',
+            '#','.','#','#','.','#','#','#','#','#','.','#','#','.','#',
+            '#','.','#','#','.','#','#','#','#','#','.','#','#','.','#',
+            '#','.','#','#','.','.','.','.','.','.','.','#','#','.','#',
+            '#','.','#','#','.','#','#','#','#','#','.','#','#','#','#',
+            '#','.','#','#','.','#','#','#','#','#','.','#','#','.','#',
+            '#','.','.','.','.','.','.','.','.','.','.','.','.','.','#',
+            '#','.','#','#','#','#','#','.','#','#','#','#','#','.','#',
+            '#','.','#','#','#','#','#','#','#','#','#','#','#','#','#',
+            '#','.','.','.','.','.','.','.','.','.','.','.','.','.','#',
+            '#','#','#','#','#','#','#','#','#','#','#','#','#','G','#'
+    };
+
+    cout<<"Vorgabe"<<endl;
+    BreathFirstSearchLib::paintMatrix(COLUMNS, ROWS, maze);
+
+    BreathFirstSearch bfs = BreathFirstSearch(COLUMNS, ROWS, maze);
+    int steps = bfs.solve();
+
+    cout<<"Loesung"<<endl;
+    printf("%d steps required \r\n", steps);
+    BreathFirstSearchLib::paintMatrix(COLUMNS, ROWS, bfs.getSolution());
+
     return 0;
+}
+
+void printString(const int ROWS, const int COLUMNS, const string &solution) {
+    int row = 0;
+    int column = 0;
+    int counter = 0;
+    for(row; row < ROWS; row++){
+        column = 0;
+        for(column; column < COLUMNS; column++){
+            cout<<solution[counter];
+            counter++;
+        }
+        cout<<endl;
+    }
 }
