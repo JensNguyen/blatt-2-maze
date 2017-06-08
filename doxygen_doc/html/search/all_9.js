e3b98618c701edfd19a0a09e865270c60458efc2
Travis CI <travis@travis-ci.org>
2017-06-08 10:32:16 +0000

    Deploy code docs to GitHub Pages Travis build: 153
    Commit: d6eadca18f2ea43298d65e95ed0589548d48a798

--- doxygen_doc/html/search/all_9.js
+++ doxygen_doc/html/search/all_9.js
@@ -4,6 +4,8 @@ var searchData=
   ['recursivemazesolvealgorithm_2ecpp',['recursivemazesolvealgorithm.cpp',['../recursivemazesolvealgorithm_8cpp.html',1,'']]],
   ['recursivemazesolvealgorithm_2eh',['recursivemazesolvealgorithm.h',['../recursivemazesolvealgorithm_8h.html',1,'']]],
   ['redraw',['redraw',['../class_maze_solve_algorithm.html#ad2903ca429a66b0f7558e169ff2c47c3',1,'MazeSolveAlgorithm']]],
+  ['reset',['reset',['../class_maze.html#aa3f5d822046f02e7a8ddf9dc2fba9a66',1,'Maze']]],
+  ['resetmaze',['resetMaze',['../class_maze_gui.html#a476e096a825384dc09d625e047a6c61b',1,'MazeGui']]],
   ['result',['Result',['../class_maze.html#a7a19b706242876f2c597033b3374e7faaa87a698c7829b0c1f1b89cc93a44f2bc',1,'Maze']]],
   ['run',['run',['../class_maze_solve_algorithm.html#a3e545c01367136ed0a02d9bb6c2b3ede',1,'MazeSolveAlgorithm']]]
 ];
--- doxygen_doc/html/search/functions_8.js
+++ doxygen_doc/html/search/functions_8.js
@@ -2,5 +2,7 @@ var searchData=
 [
   ['recursivemazesolvealgorithm',['RecursiveMazeSolveAlgorithm',['../class_recursive_maze_solve_algorithm.html#a62a60de4fb81a35cae475eabc5a5a66e',1,'RecursiveMazeSolveAlgorithm']]],
   ['redraw',['redraw',['../class_maze_solve_algorithm.html#ad2903ca429a66b0f7558e169ff2c47c3',1,'MazeSolveAlgorithm']]],
+  ['reset',['reset',['../class_maze.html#aa3f5d822046f02e7a8ddf9dc2fba9a66',1,'Maze']]],
+  ['resetmaze',['resetMaze',['../class_maze_gui.html#a476e096a825384dc09d625e047a6c61b',1,'MazeGui']]],
   ['run',['run',['../class_maze_solve_algorithm.html#a3e545c01367136ed0a02d9bb6c2b3ede',1,'MazeSolveAlgorithm']]]
 ];
